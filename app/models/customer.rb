d3cc1c484670f389925fe506eb034a07f4b958ae
Michael OBrien <obrien.michael.t@gmail.com>
2024-11-01 18:09:19 -0400

    Moved db logic from controller to model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,7 @@
 class Customer < ApplicationRecord
   has_many :invoices
+
+  def self.customersForMerchant(merchant)
+    Customer.joins(:invoices).where("merchant_id = ?", merchant).uniq
+  end
 end
