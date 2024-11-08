942970fd6dfc864b6cd4f5e206ca243bd13cb9bf
Jeremiah Ross <crosswolf.20work@gmail.com>
2024-11-08 13:34:39 -0600

    added validation for status

--- spec/models/coupon_spec.rb
+++ spec/models/coupon_spec.rb
@@ -11,6 +11,7 @@ RSpec.describe Coupon, type: :model do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:description) }
     it { should validate_presence_of(:percent_off || :flat_value ) }
+    it { should validate_presence_of(:status) }
     it { should validate_presence_of(:merchant_id) }
   end
 end
