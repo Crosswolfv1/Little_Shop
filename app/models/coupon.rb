5fac0222451cfaf61ae2222dad8b3edc435208a0
Jeremiah Ross <crosswolf.20work@gmail.com>
2024-11-08 13:10:13 -0600

    added validations for fields in coupon objects

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,5 +1,12 @@
 class Coupon < ApplicationRecord
   belongs_to :merchant
   has_one :invoice
+
+  validates_presence_of :name, :presence => true
+  validates_presence_of :description, :presence => true
+  validates_presence_of :percent_off, unless: -> { dollar_off.present? }
+  validates_presence_of :dollar_off, unless: -> { percent_off.present? }
+  validates_presence_of :merchant_id, :presence => true
+
 end
 
