800d5bce44b83cc9855324d5bcee496f61a27e79
Jeremiah Ross <crosswolf.20work@gmail.com>
2024-11-10 01:34:24 -0600

    added attribute coupon count and invoice coupon count

--- app/serializers/merchant_index_serializer.rb
+++ app/serializers/merchant_index_serializer.rb
@@ -7,4 +7,16 @@ class MerchantIndexSerializer
 	attribute :item_count do |merchant|
 		merchant.items.count
 	end
+
+	attribute :coupon_count do |merchant|
+		coupons = Coupon.where(merchant_id: merchant.id)
+		if coupons.empty?
+			0
+		end
+	end
+
+	attribute :invoice_coupon_count do |merchant|
+		all_coupons = Coupon.where(merchant_id: merchant.id)
+		Invoice.where(coupon_id: all_coupons.pluck(:id)).count
+	end
 end
