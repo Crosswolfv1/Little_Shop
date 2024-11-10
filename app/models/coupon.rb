eddb58fe89880de69f2e220e31a47fc16fffa95e
Jeremiah Ross <crosswolf.20work@gmail.com>
2024-11-10 11:48:24 -0600

    added class method for filtering

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -9,5 +9,14 @@ class Coupon < ApplicationRecord
   validates_presence_of :code, :presence => true
   validates_presence_of :status, :presence => true
   validates_presence_of :merchant_id, :presence => true
+
+  def self.filter_coupons(scope, params)
+    filter = params[:status]
+    if params[:status].present?
+      scope.where("status = ?", "#{filter}")
+    else 
+      scope
+    end
+  end
 end
 
