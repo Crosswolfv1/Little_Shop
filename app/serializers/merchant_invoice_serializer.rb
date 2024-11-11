fa01929612aed065e19603435d953924aadeacdb
Jeremiah Ross <crosswolf.20work@gmail.com>
2024-11-11 16:14:59 -0600

    added view for rice after discounting to serializer

--- app/serializers/merchant_invoice_serializer.rb
+++ app/serializers/merchant_invoice_serializer.rb
@@ -3,4 +3,9 @@ class MerchantInvoiceSerializer
     set_id :id
     set_type :invoice
     attributes :customer_id, :merchant_id, :status, :coupon_id 
+
+    attribute :discounted_total do |invoice|
+        merchant = invoice.merchant
+        Invoice.calculate_discounted_total(invoice.id, merchant.id)
+    end
 end
